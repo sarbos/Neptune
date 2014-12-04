ff2c43b697de033a783b714ae8c8e07bba7ff182
Jazmine Olinger <olingj@rpi.edu>
2014-12-04 17:05:58 -0500

    added space for video feed

--- interface.py
+++ interface.py
@@ -1,4 +1,8 @@
 import Tkinter
 root = Tkinter.Tk()
-root.minsize(1000,1000)
+root.minsize(900,700)
+
+canvas = Tkinter.Canvas(root, height = 1000, width = 1000);
+rect = canvas.create_rectangle(10,10,990,650,fill="light gray")
+canvas.pack();
 root.mainloop()
